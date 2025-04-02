63c74f8a391c8c09bd3a4a58b616dbd09f262a18
Carlos <capmatera@gmail.com>
2025-04-02 20:12:58 -0300

    Segunda alteracao

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// import 'package:todoapp/app/home/home_page.dart';
+import 'package:todoapp/features/home/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supermercado',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: const HomePage(),
+      // home:  HomePage(title: 'Tauste', onToggleTheme: (){},),
+    );
+  }
+}
